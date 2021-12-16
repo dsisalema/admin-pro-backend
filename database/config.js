0ed65e5a7e54eb0b1ed187f0060554537d0a1f5f
David Sisalema <dsisalema@gestor.co>
2021-12-16 17:21:08 -0500

    CORS y Express - primer commit

---
+++ database/config.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async () => {
+
+    try {
+        //await mongoose.connect('mongodb+srv://david1975:david1975@cluster0.gwy9h.mongodb.net/hospitaldb');
+        await mongoose.connect(process.env.DB_CNN);
+
+        console.log('DB Online');
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error a la hora de iniciar la DB ver los');
+    }
+}
+
+
+module.exports = {
+    dbConnection
+}
