6e9e6480daf4e3334b437e2949febbc1addf0f01
David Sisalema <dsisalema@gestor.co>
2022-01-08 17:56:03 -0500

    Fin sección 12 - Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async (req, res = response) => {
@@ -51,6 +52,60 @@ const login = async (req, res = response) => {
 
 }
 
+
+const googleSignIn = async (req, res = response) => {
+
+    const googleToken = req.body.token;
+
+    try {
+        
+        const { name, email, picture } = await googleVerify(googleToken);
+
+        //validar si existe el usuario con ese correo
+        const usuarioDB = await Usuario.findOne({email});
+        let usuario;
+        if ( !usuarioDB) {
+            //Sino existe el usuario
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: '@mail',
+                img: picture,
+                google: true
+
+            });
+        } else{
+            //Existe usuario
+            usuario = usuarioDB;
+            usuario.google = true;
+            //usuario.password = '@@@';
+
+        }
+        //Guardar en base de datos
+        await usuario.save();
+        //Generar un token
+        const token = await generarJWT(usuario.id );
+
+        res.status(500).json({
+            ok: true,
+            token
+        });
+
+    } catch (error) {
+        
+        res.status(401).json({
+            ok: trfalseue,
+            msg: 'Token no es correcto'
+    
+        });
+
+    }
+
+
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
